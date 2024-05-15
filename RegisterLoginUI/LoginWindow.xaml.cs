7fc23efecc7b3f69e6262c37f8ab4cd7cad070c8
xDeserek_ <dawiddominik@interia.eu>
2024-05-15 13:42:44 +0200

    Add project files.

---
+++ RegisterLoginUI/LoginWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RegisterLoginUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void BtnCreateAccount_Click(object sender, RoutedEventArgs e)
+        {
+            new RegisterWindow().Show();
+            this.Close();
+        }
+
+        private void Login_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private bool IsClickInsideControl(Control control, MouseButtonEventArgs e)
+        {
+            Point mousePosition = e.GetPosition(control);
+            return mousePosition.X >= 0 && mousePosition.X <= control.ActualWidth &&
+                   mousePosition.Y >= 0 && mousePosition.Y <= control.ActualHeight;
+        }
+
+        private void Window_PrevieMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!IsClickInsideControl(TxtUsernameLogin, e) && !IsClickInsideControl(TxtPasswordLogin, e))
+            {
+                Keyboard.ClearFocus();
+            }
+        }
+    }
+}
---
+++ RegisterLoginUI/RegisterWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RegisterLoginUI
+{
+    /// <summary>
+    /// Interaction logic for RegisterWindow.xaml
+    /// </summary>
+    public partial class RegisterWindow : Window
+    {
+        public RegisterWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBackToLogin_Click(object sender, RoutedEventArgs e)
+        {
+            new LoginWindow().Show();
+            this.Close();
+        }
+
+        private void ButtonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Register_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void Window_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (!IsClickInsideControl(TxtUsernameRegister, e) && !IsClickInsideControl(TxtPasswordRegister, e)
+                && !IsClickInsideControl(TxtEmailRegister, e) && !IsClickInsideControl(TxtConfirmPasswordRegister, e))
+            {
+                Keyboard.ClearFocus();
+            }
+        }
+
+        private bool IsClickInsideControl(Control control, MouseButtonEventArgs e)
+        {
+            Point mousePosition = e.GetPosition(control);
+            return mousePosition.X >= 0 && mousePosition.X <= control.ActualWidth &&
+                   mousePosition.Y >= 0 && mousePosition.Y <= control.ActualHeight;
+        }
+    }
+}
